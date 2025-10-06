7bba1c436258bf2497271d2d577a417ca4eff93b
unknown <lfvaldez.it@gmail.com>
2025-10-06 07:14:00 -0300

    Add description

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -24,32 +24,38 @@ variable "common_tags" {
   }
 
   variable "r53_resolver_in_ip_1" {
+    description = "R53 IN resolver IP 1"
     type = string
     default = "null"
   }
 
   variable "r53_resolver_in_ip_2" {
+        description = "R53 IN resolver IP 2"
     type = string
     default = "null"
   }
 
   variable "target_domain_name" {
+    description = "Target domain name"
     type = string
     default = ""
   }
 
   variable "target_ip_primary" {
+    description = "Target IP primary"
     type = string
     default = "null"
   }
 
   variable "target_ip_secondary" {
+      description = "Target IP secondary"
     type = string
     default = "null"
   }
 
 
 variable "subnets" {
+  description = "List of subnets"
   type = list(object({
     name                  = string
     cidr_block            = string
